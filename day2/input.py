c657d81493ff4c3c0d032668a44ef8b311332497
Nyaradzo20 <nchinyati20@gmail.com>
2021-12-09 11:04:07 +0000

    inputs

---
+++ day2/input.py
@@ -0,0 +1,6 @@
+#Using input() function ask for user's name.
+ans_1=input("your name please")
+ 
+print("Hello!, " + ans_1)
+
+
---
+++ day2/input2.py
@@ -0,0 +1,6 @@
+#This time ask the user a numerical question, such as, "Please enter your age." See what type of data input() returns.
+
+
+ans_1= input("please enter your age")
+print(type(ans_1))
+
